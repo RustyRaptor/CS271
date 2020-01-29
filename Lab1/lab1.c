2b09259ad1b9471de706f7ce7486fb3004001736
RustyRaptor <ziada@nmsu.edu>
2020-01-28 17:12:25 -0700

    Lab1 finished

---
+++ Lab1/lab1.c
@@ -0,0 +1,47 @@
+// CS 271 - Lab 1
+// Program name: lab1.c
+// Ziad Arafat
+// 01/28/2020
+
+#include <stdio.h>
+#include <math.h>
+
+
+// A program to count occurences of a digit in a number
+
+
+
+int main (void) {
+    // digit initialized to -1 for first while loop to work
+    // tempnum used to store number for printing later
+    
+    int digit = -1;
+    int number, count, tempnum;
+    
+    
+    // gets a 1 digit number from the user and prompts till satisfied
+    while (digit > 9 || digit < 0) {
+        printf("Enter a 1 digit number to search for");
+        scanf("%d", &digit);
+    } // end loop
+    
+    // get the number to count in
+    printf("Please enter a number");
+    scanf("%d", &number);
+    tempnum = number;
+    
+    // Checks against the modulo by 10 as that gives
+    // us the last digit in the number
+    // Then we divide by 10. Since it's integer division it will just
+    // remove the last digit.
+    while (number > 0){
+        if (digit == number%10)
+            count++;
+        
+        number=number/10;
+    } // end loop
+    
+    // Print out all the info
+    printf("The digit (%d) appears %d times in the number (%d)\n",
+           digit, count, tempnum);
+} // end main function
