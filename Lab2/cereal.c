a58cb81514cba8ae53902f5a71cb6b6e4768b86c
Ziad Arafat <ziada@nmsu.edu>
2020-02-10 19:40:54 -0700

    more changes to lab 2

--- Lab2/cereal.c
+++ Lab2/cereal.c
@@ -10,82 +10,87 @@
 #include <string.h>
 
 int main( void ) {
-  int min_protein;
-  int max_calories;
-  int max_sugar;
+     int minProtein;
+     int maxCalories;
+     int maxSugar;
 
-  // get constraint values
+     // get constraint values
 
-  // get protein
-  printf("input the minimum number of grams of protein\n");
-  scanf("%d", &min_protein);
+     // get protein
+     printf( "input the minimum number of grams of protein\n" );
+     scanf( "%d", &minProtein );
 
-  // get calories
-  printf("input the maximum number of calories\n");
-  scanf("%d", &max_calories);
+     // get calories
+     printf( "input the maximum number of calories\n" );
+     scanf( "%d", &maxCalories );
 
-  // get sugar
-  printf("input the maximum number of grams of sugar\n");
-  scanf("%d", &max_sugar);
+     // get sugar
+     printf( "input the maximum number of grams of sugar\n" );
+     scanf( "%d", &maxSugar );
 
-  printf("Here are the cereals that match your criteria:\n\n");
+     printf( "Here are the cereals that match your criteria:\n\n" );
 
-  char name[23], compname[15];
-  char company = ' ';
-  int cals = 0, protein = 0, sugar = 0, count = 0;
+     char name[23], companyName[15];
+     char company = ' ';
+     int cals = 0, protein = 0, sugar = 0, count = 0;
 
-  // declare our file to read from
-  FILE *filePtr;
+     // declare our file to read from
+     FILE *filePtr;
 
-  // ensure the file exists.
-  if ((filePtr = fopen("./cereal.txt", "r")) == NULL)
-    printf("File could not be opened\n");
+     // ensure the file exists.
+     if ( ( filePtr = fopen( "./cereal.txt", "r" ) ) == NULL ){
+          printf( "File could not be opened\n" );
+     } // end if
+         
 
-  else {
-    while (!feof(filePtr)) {
-      // read in all the needed data from the line
-      fscanf(filePtr, "%s %c %d %d %*d %*d %*f %*f %d",
-              name, &company, &cals, &protein, &sugar);
+     else {
+          while ( !feof( filePtr ) ) {
 
-      // We must break the loop because it will read one more line otherwise
-      if (feof(filePtr)) {
-        printf("%d cereals match your criteria.\n", count);
-        break;
-      } // end if
+               // read in all the needed data from the line
+               fscanf( filePtr, "%s %c %d %d %*d %*d %*f %*f %d", name,
+                       &company, &cals, &protein, &sugar );
 
-      // Set the full company name based on the letter
-      switch (company) {
-        case 'G':
-          strcpy(compname, "General Mills");
-          break;
-        case 'K':
-          strcpy(compname, "Kellogs");
-          break;
-        case 'Q':
-          strcpy(compname, "Quaker Oats");
-          break;
-        // default in case a different brand is added, also useful for
-        // debugging.
-        default:
-          strcpy(compname, "Generic");
-          break;
-      } // end switch
+               // We must break the loop because it will read one more line 
+               // otherwise
+               if ( feof( filePtr ) ) {
+                    printf( "%d cereals match your criteria.\n", count );
+                    break;
+               } // end if
 
-      // if the cereal meets the requirements, print it and increase count.
-      if (cals <= max_calories && protein >= min_protein &&
-          sugar <= max_sugar) {
-        count++;
+               // Set the full company name based on the letter
+               switch ( company ) {
+               case 'G':
+                    strcpy( companyName, "General Mills" );
+                    break;
+               case 'K':
+                    strcpy( companyName, "Kellogs" );
+                    break;
+               case 'Q':
+                    strcpy( companyName, "Quaker Oats" );
+                    break;
 
-        // print out the values for each cereal that matches. 
-        printf(
-            "%-15s%-24s%4d calories\n"
-            "%15s%-24s%4d grams\n"
-            "%15s%-24s%4d grams\n\n",
-            compname, name, cals,
-            "", "  - protein", protein,
-            "", "  - sugar", sugar);
+               // default in case a different brand is added, also useful for 
+               // debugging.
+               default:
+                    strcpy( companyName, "Generic" );
+                    break;
+               } // end switch
 
-      } // end if
-    } // end while
-  } // end else
-}
+               // if the cereal meets the requirements, print it and increase 
+               // count.
+               if ( cals <= maxCalories && protein >= minProtein &&
+                    sugar <= maxSugar ) {
+                    count++;
+
+                    // print out the values for each cereal that matches.
+                    printf( "%-15s%-24s%4d calories\n"
+                            "%15s%-24s%4d grams\n"
+                            "%15s%-24s%4d grams\n\n",
+                            companyName, name, cals,
+                            "", "  - protein", protein,
+                            "", "  - sugar", sugar );
+
+               } // end if
+          } // end while
+     } // end else
+} // end main
