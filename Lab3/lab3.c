02d9821db761e48ea89118d95a37e96fba635741
Ziad Arafat <ziada@nmsu.edu>
2020-02-17 20:49:59 -0700

    finished lab3

--- Lab3/lab3.c
+++ Lab3/lab3.c
@@ -4,20 +4,23 @@
 //  written by: Ziad Arafat
 //  Date Written: 2020-02-17
 
+
+// Why does it ask us to include the header file here? Shouldn't we be including
+// the functions C file? It won't work otherwise. 
+// #include "lab3functions.h"
 #include "lab3functions.c"
 #include <stdio.h>
 
 int main( void ) {
-
      // test factors
-     factors(16, 8);
-     factors(12, 20);
+     factors( 16, 8 );
+     factors( 12, 20 );
 
      // test timeDisplay
-     timeDisplay(25503);
-     timeDisplay(25678);
+     timeDisplay( 25503 );
+     timeDisplay( 25678 );
 
      // test rollingDice
-     rollingDice(20);
-     rollingDice(5678);
+     rollingDice( 20 );
+     rollingDice( 5678 );
 }
--- Lab3/lab3functions.h
+++ Lab3/lab3functions.h
@@ -6,7 +6,9 @@
 
 #ifndef LAB3FUNCTIONS
 #define LAB3FUNCTIONS
+
 void factors( int, int );
 void timeDisplay( int );
-void rollingDice( int n);
+void rollingDice( int n );
+
 #endif
